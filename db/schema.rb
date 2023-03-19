416863b492477b91c55d27c9cc8fdc14be25c91b
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-19 19:25:11 +0800

    category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_14_130121) do
+ActiveRecord::Schema.define(version: 2023_03_18_143839) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "plays", force: :cascade do |t|
     t.string "title"
@@ -19,6 +25,7 @@ ActiveRecord::Schema.define(version: 2023_03_14_130121) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
